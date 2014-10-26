af20b9aee533c2a911b7e477513e4422659c7fe8
Erik Lövdahl <erik.lovdahl@gmail.com>
2014-10-26 15:34:14 +0100

    Add ugly jump animation

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,15 +3,30 @@ using System.Collections;
 
 public class Character : MonoBehaviour {
 	public float defaultRotation = 78f;
+	public float jumpForce;
+	public float speed;
+
 	private float _rotation;
+	private Animator _animator;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		_animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		_rotation = transform.rotation.eulerAngles.z;
+		if (Input.GetButtonDown("Fire1")) {
+			Debug.Log ("asd");
+			_animator.SetTrigger("Fly");
+			this.rigidbody2D.AddForce(Vector2.up * jumpForce);
+		}
+	}
+
+	void FixedUpdate() {
+		if (this.rigidbody2D.velocity.x < speed) {
+			this.rigidbody2D.AddForce(Vector2.right * 100);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
